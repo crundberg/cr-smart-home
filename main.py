9e64308b085366ccc26abad35ff122bbb8d01292
crundberg <christoffer@crwebb.se>
2015-11-29 19:42:30 +0100

    The lamp was extinguished at midnight

--- main.py
+++ main.py
@@ -32,20 +32,21 @@ for lamp in LampObjects:
 	n+=1
 	print '%d: %s (%s), Start: %s, Stop: %s, Active: %d' % (n, lamp.Name, lamp.IO, lamp.TimeStart, lamp.TimeStop, lamp.Active)
 
+print ''
+
 #Run program
 while True:
 	#Datetime
 	now = datetime.datetime.now()
-	sunset = SunriseSunset.sunset(now, latitude, longitude)
 	sunrise = SunriseSunset.sunrise(now, latitude, longitude)
+	sunset = SunriseSunset.sunset(now, latitude, longitude)
 	dark = (now < sunrise or now > sunset)
 
-	print ''
-	print 'Time: %02d:%02d, Dark: %d, Sunset: %s, Sunrise: %s' % (now.hour, now.minute, dark, sunset, sunrise)
+	print 'Time: %02d:%02d, Dark: %d, Sunrise: %s, Sunset: %s' % (now.hour, now.minute, dark, sunrise, sunset)
 
-	#Loop objects
+	#Loop lamps
 	for lamp in LampObjects:
-		#Start and stop time for lamp
+		#Start and stop time for today
 		sStart = '%s %s' % (now.date(), lamp.TimeStart)
 		sStop = '%s %s' % (now.date(), lamp.TimeStop)
 		dtStart = datetime.datetime.strptime(sStart, '%Y-%m-%d %H:%M')
@@ -54,33 +55,42 @@ while True:
 		if (dtStart > dtStop):
 			dtStop += datetime.timedelta(days=1)
 
-		bStart = (now > dtStart and now < dtStop)
+		#Start and stop time for yesterday
+		sStart = '%s %s' % (now.date() - datetime.timedelta(days=1), lamp.TimeStart)
+		sStop = '%s %s' % (now.date() - datetime.timedelta(days=1), lamp.TimeStop)
+		dtStart2 = datetime.datetime.strptime(sStart, '%Y-%m-%d %H:%M')
+		dtStop2 = datetime.datetime.strptime(sStop, '%Y-%m-%d %H:%M')
 
-		#Object inactive
+
+		if (dtStart2 > dtStop2):
+			dtStop2 += datetime.timedelta(days=1)
+
+
+		bStart = ((now > dtStart and now < dtStop) or (now > dtStart2 and now < dtStop2))
+
+		#Lamp inactive
 		if (lamp.Active == 0):
-			print '%s is inactivate' % lamp.Name
+			#print '%s is inactivate' % lamp.Name
 			continue
 		
 		#Start lamp
 		if (bStart and dark):
 			if (lamp.Started == 0):
-				print 'Starting %s' % lamp.Name
+				print '%s: Starting %s' % (now, lamp.Name)
 				lamp.Started = 1
 				continue
 			else:
-				print '%s is already started' % lamp.Name
-				lamp.Started = 1
+				#print '%s is already started' % lamp.Name
 				continue	
 
 		#Stop lamp
 		if (not bStart or not dark):
 			if (lamp.Started == 1):
-				print 'Stoping %s' % lamp.Name
+				print '%s: Stoping %s' % (now, lamp.Name)
 				lamp.Started = 0
 				continue
 			else:
-				print '%s is already stoped' % lamp.Name
-				lamp.Started = 0
+				#print '%s is already stoped' % lamp.Name
 				continue
 
 	#Sleep 60s
